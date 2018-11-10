f551a1b472890e880fa579e579e46dba25b537d6
Igor Halfeld <igor@devstacks.com.br>
2018-11-10 18:02:33 -0200

    Initial commit

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>My to-do list!</h1>
+    <input
+      v-model="currentValue"
+      type="text"
+      placeholder="Add task..."
+      @keyup.enter="addTask()"
+      class="container-input">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <input type="checkbox" v-model="item.isDone">
+        <template v-if="item.isDone">
+          <s>{{ item.name }}</s>
+        </template>
+        <template v-else>
+          {{ item.name }}
+        </template>
+        <button @click="removeTask(index)">delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    currentValue: '',
+    items: [{
+      name: 'First item',
+      isDone: false
+    }]
+  }),
+  methods: {
+    addTask () {
+      this.items.push({
+        name: this.currentValue,
+        isDone: false
+      })
+      this.currentValue = '';
+    },
+    removeTask (index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style src="./style.css"></style>
---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <h2 align="center">
+    Simple to-do app demo to <a href="https://fatecdevday.com.br">Fatec dev day</a>
+  </h2>
+  <h3 align="center">
+    <a href="https://speakerdeck.com/igorhalfeld/nativescript-vue">Slides</a>
+  </h3>
+</p>
+
+![screenshot](./screen.png)
+
+### Run
+
+```sh
+npm install -g @vue/cli vue-cli-se
+```
